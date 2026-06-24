c87b2718d51b87e92476440fbbf2fc90bbf16290
Thales Oliveira <TOliveiraDev@outlook.com>
2026-06-23 21:52:18 -0300

    build: Update angular version to 22.0.3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Hello } from "./components/hello/hello";
+import { Hello } from './components/hello/hello';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Hello],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('modern-angular-zero-to-advanced--loiane-groner');
