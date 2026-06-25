f2fa6a76a9d92b0d7c14300e1a923e74c0b21cb2
Thales Oliveira <TOliveiraDev@outlook.com>
2026-06-25 17:09:21 -0300

    feat: aula 9 - cria o componente de grid de produtos com o componente de card mockado

---
+++ src/app/components/products/products-grid/products-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductCard } from '../product-card/product-card';
+
+@Component({
+  selector: 'app-products-grid',
+  imports: [ProductCard],
+  templateUrl: './products-grid.html',
+  styleUrl: './products-grid.scss',
+})
+export class ProductsGrid {
+}
