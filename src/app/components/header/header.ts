cf29ddd12fe22ca4a40465d8cb8a24756a2274a8
Thales Oliveira <TOliveiraDev@outlook.com>
2026-07-02 18:44:14 -0300

    feat: aula 17 - implementa um contador de itens no carrinho por meio de um matBadge

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CartService } from '../../services/cart/cart-service';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatBadgeModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  protected readonly cartService = inject(CartService);
+
+}
