60a3596ba49b8cea650f4694fde13f789b01f4d5
Thales Oliveira <TOliveiraDev@outlook.com>
2026-07-02 18:43:15 -0300

    refactor: aula 17 - refatora o cartService para utilizar o cardItem e implementa o signal totalItems

--- src/app/services/cart/cart-service.ts
+++ src/app/services/cart/cart-service.ts
@@ -1,13 +1,27 @@
+import { CartItem } from '../../interfaces/cart/cart-item';
 import { Product } from './../../components/products/product';
-import { Service, signal } from '@angular/core';
+import { computed, Service, signal } from '@angular/core';
 
 @Service()
 export class CartService {
 
-  private readonly cartItems = signal<Product[]>([]);
+  private readonly cartItems = signal<CartItem[]>([]);
 
-  addToCard(product: Product) {
-    console.log('Produto adicionado ao carrinho:', product);
-    this.cartItems.update((items) => [...items, product]); // Retorna uma nova instância, ao invés do push que não retorna.
+  readonly totalItems = computed(() => 
+    this.cartItems().reduce((total, item) => total + item.quantity, 0)
+  )
+
+  addToCard(product: Product): void {
+    this.cartItems.update((items) => {
+      const existingItem = items.find((item) => item.product.id === product.id);
+
+      if (existingItem) {
+        return items.map((item) =>
+          item.product.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      };
+
+      return [...items, { product, quantity: 1}]
+    });
   };
 }
