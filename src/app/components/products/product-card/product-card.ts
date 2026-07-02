5f96e2297eabc787a1f129803211d02749801e94
Thales Oliveira <TOliveiraDev@outlook.com>
2026-07-02 11:40:15 -0300

    feat: aula 15 - implementa evento de output para ao clicar no botão para adicionar ao carrinho

--- src/app/components/products/product-card/product-card.ts
+++ src/app/components/products/product-card/product-card.ts
@@ -1,5 +1,5 @@
 import { Product } from './../product';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
@@ -16,4 +16,9 @@ export class ProductCard {
 
   readonly product = input.required<Product>();
   readonly addButtonLabel = input('Adicionar ao carrinho');
+  readonly addToCart = output<Product>();
+
+  protected onAddToCard() {
+    this.addToCart.emit(this.product());
+  }
 }
